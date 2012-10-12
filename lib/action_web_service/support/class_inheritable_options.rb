8c87c5776e70e28afa47c62f14846de0ea3f9492
Jens Kraemer <jk@jkraemer.net>
2012-10-12 16:10:10 +0200

    fix

--- lib/action_web_service/support/class_inheritable_options.rb
+++ lib/action_web_service/support/class_inheritable_options.rb
@@ -1,5 +1,4 @@
-require 'active_support/core_ext/class/attribute'
-require 'inheritable_attributes'
+require 'active_support/core_ext/class/inheritable_attributes'
 class Class # :nodoc:
   def class_inheritable_option(sym, default_value=nil)
     write_inheritable_attribute sym, default_value
